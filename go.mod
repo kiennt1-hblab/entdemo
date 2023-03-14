db1e867c35ced12f25e06f12b227b7c7beaf4e80
kiennt1 <kiennt1@hblab.vn>
2023-03-14 10:17:14 +0700

    clear data

--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module entdemo
 go 1.18
 
 require (
-	entgo.io/ent v0.11.4
+	entgo.io/ent v0.11.10
 	github.com/gin-gonic/gin v1.8.1
-	github.com/go-sql-driver/mysql v1.6.0
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/lib/pq v1.10.7
 )
 
 require (
-	ariga.io/atlas v0.7.3-0.20221011160332-3ca609863edd // indirect
+	ariga.io/atlas v0.9.2-0.20230303073438-03a4779a6338 // indirect
 	github.com/agext/levenshtein v1.2.1 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -31,10 +32,10 @@ require (
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/zclconf/go-cty v1.8.0 // indirect
 	golang.org/x/crypto v0.3.0 // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/net v0.2.0 // indirect
-	golang.org/x/sys v0.2.0 // indirect
-	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.6.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,35 +2,27 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"entdemo/ent"
+	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-	"time"
-
-	"entdemo/ent"
-	"entdemo/ent/car"
-	"entdemo/ent/user"
-	"github.com/gin-gonic/gin"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	client, err := ent.Open("mysql", "yuki:#Yuk1@23@tcp(localhost:3306)/sample_golang?parseTime=True")
+	client, err := ent.Open("postgres", "host=localhost port=5432 user=postgres dbname=entdemo password=123456")
 	if err != nil {
-		log.Fatalf("failed opening connection to mysql: %v", err)
+		log.Fatalf("failed opening connection to postgres: %v", err)
 	}
 	defer client.Close()
 	// Run the auto migration tool.
 	ctx := context.Background()
 
-	if err := client.Schema.Create(ctx); err != nil {
+	if err = client.Schema.Create(ctx); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
-
-	//if err := QueryCars(ctx, a8m); err != nil {
-	//	log.Fatal(err)
-	//}
 
 	router := gin.Default()
 
@@ -38,54 +30,6 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "success",
 		})
-	})
-	router.POST("/create-user", func(c *gin.Context) {
-		name := c.PostForm("name")
-		if _, err = CreateUser(ctx, client, name); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": err.Error(),
-			})
-		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "success",
-			})
-		}
-	})
-	router.GET("/query-user", func(c *gin.Context) {
-		name := c.Query("name")
-		u, err := QueryUser(ctx, client, name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		c.JSON(http.StatusOK, gin.H{
-			"message": u,
-		})
-	})
-	router.POST("/create-car", func(c *gin.Context) {
-		name := c.PostForm("name")
-		a8m, err := CreateCars(ctx, client, name)
-		if err != nil {
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"message": a8m,
-			})
-		}
-	})
-	router.GET("/list-car", func(c *gin.Context) {
-		name := c.Query("name")
-		cars, err := QueryCars(ctx, client, name)
-		if err != nil {
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			c.JSON(http.StatusOK, gin.H{
-				"message": cars,
-			})
-		}
 	})
 
 	//router.PUT("/somePut", putting)
@@ -98,174 +42,4 @@ func main() {
 	// PORT environment variable was defined.
 	router.Run()
 	// router.Run(":3000") for a hard coded port
-}
-
-func CreateUser(ctx context.Context, client *ent.Client, name string) (*ent.User, error) {
-	u, err := client.User.
-		Create().
-		SetAge(30).
-		SetName(name).
-		Save(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed creating user: %w", err)
-	}
-	log.Println("user was created: ", u)
-	return u, nil
-}
-
-func QueryUser(ctx context.Context, client *ent.Client, name string) (*ent.User, error) {
-	u, err := client.User.
-		Query().
-		Where(user.NameEQ(name)).
-		// `Only` fails if no user found,
-		// or more than 1 user returned.
-		Only(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed querying user: %w", err)
-	}
-	log.Println("user returned: ", u)
-	return u, nil
-}
-
-func CreateCars(ctx context.Context, client *ent.Client, name string) (*ent.User, error) {
-	// Create a new car with model "Tesla".
-	tesla, err := client.Car.
-		Create().
-		SetModel(name).
-		SetRegisteredAt(time.Now()).
-		Save(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed creating car: %w", err)
-	}
-	log.Println("car was created: ", tesla)
-
-	// Create a new car with model "Ford".
-	ford, err := client.Car.
-		Create().
-		SetModel("Ford").
-		SetRegisteredAt(time.Now()).
-		Save(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed creating car: %w", err)
-	}
-	log.Println("car was created: ", ford)
-
-	// Create a new user, and add it the 2 cars.
-	a8m, err := client.User.
-		Create().
-		SetAge(30).
-		SetName("Ta8ms").
-		AddCars(tesla, ford).
-		Save(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed creating user: %w", err)
-	}
-	log.Println("user was created: ", a8m)
-	return a8m, nil
-}
-
-func QueryCars(ctx context.Context, client *ent.Client, name string) ([]*ent.Car, error) {
-	cars, err := client.Car.Query().All(ctx)
-	user, _ := QueryUser(ctx, client, name)
-	if err != nil {
-		return nil, fmt.Errorf("failed querying user cars: %w", err)
-	}
-	log.Println("returned cars:", cars)
-
-	// What about filtering specific cars.
-	ford, err := user.QueryCars().
-		Where(car.Model("Ford")).
-		Only(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed querying user cars: %w", err)
-	}
-	log.Println(ford)
-	return cars, nil
-}
-
-func QueryCarUsers(ctx context.Context, a8m *ent.User) error {
-	cars, err := a8m.QueryCars().All(ctx)
-	if err != nil {
-		return fmt.Errorf("failed querying user cars: %w", err)
-	}
-
-	// Query the inverse edge.
-	for _, c := range cars {
-		owner, err := c.QueryOwner().Only(ctx)
-		if err != nil {
-			return fmt.Errorf("failed querying car %q owner: %w", c.Model, err)
-		}
-		log.Printf("car %q owner: %q\n", c.Model, owner.Name)
-	}
-	return nil
-}
-
-func CreateGraph(ctx context.Context, client *ent.Client) error {
-	// First, create the users.
-	a8m, err := client.User.
-		Create().
-		SetAge(30).
-		SetName("Ariel").
-		Save(ctx)
-	if err != nil {
-		return err
-	}
-	neta, err := client.User.
-		Create().
-		SetAge(28).
-		SetName("Neta").
-		Save(ctx)
-	if err != nil {
-		return err
-	}
-	// Then, create the cars, and attach them to the users created above.
-	err = client.Car.
-		Create().
-		SetModel("Tesla").
-		SetRegisteredAt(time.Now()).
-		// Attach this car to Ariel.
-		SetOwner(a8m).
-		Exec(ctx)
-	if err != nil {
-		return err
-	}
-	err = client.Car.
-		Create().
-		SetModel("Mazda").
-		SetRegisteredAt(time.Now()).
-		// Attach this car to Ariel.
-		SetOwner(a8m).
-		Exec(ctx)
-	if err != nil {
-		return err
-	}
-	err = client.Car.
-		Create().
-		SetModel("Ford").
-		SetRegisteredAt(time.Now()).
-		// Attach this graph to Neta.
-		SetOwner(neta).
-		Exec(ctx)
-	if err != nil {
-		return err
-	}
-	// Create the groups, and add their users in the creation.
-	err = client.Group.
-		Create().
-		SetName("GitLab").
-		AddUsers(neta, a8m).
-		Exec(ctx)
-	if err != nil {
-		return err
-	}
-	err = client.Group.
-		Create().
-		SetName("GitHub").
-		AddUsers(a8m).
-		Exec(ctx)
-	if err != nil {
-		return err
-	}
-	log.Println("The graph was created successfully")
-	return nil
 }
